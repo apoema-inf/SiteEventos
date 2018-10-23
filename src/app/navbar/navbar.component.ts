37b311b9372a47dd56e63ea816b1d81291feb1d8
hyagosouzza <hyagosouzza@hotmail.com>
2018-10-23 10:19:15 -0300

    aplicando navbar-mobile e navbar-fixed with color

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $:any;
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -10,6 +12,12 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function () {
+      $(document).scroll(function () {
+        var $nav = $(".linear");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      });
+    });
   }
 
 }
