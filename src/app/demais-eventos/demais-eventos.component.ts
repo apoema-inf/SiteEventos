98a54b1a1eb2bb605fd28ff783338925d70021f1
Karine Pires <karaujo@asert.com.br>
2019-02-28 08:21:00 -0300

    Correcao

--- src/app/demais-eventos/demais-eventos.component.ts
+++ src/app/demais-eventos/demais-eventos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-demais-eventos',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DemaisEventosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   eventos = [
     {
@@ -41,6 +42,10 @@ export class DemaisEventosComponent implements OnInit {
   ]
 
   ngOnInit() {
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
